7f9abf6b1fc7a6dfe87cd241c89e533d5a9b3423
blaxk <dev@blaxk.com>
2020-12-15 19:37:26 +0900

    RemainTimer 시간값 보정하도록 수정

--- src/utils/RemainTimer.js
+++ src/utils/RemainTimer.js
@@ -15,12 +15,10 @@ ixBand.utils.RemainTimer = $B.Class.extend({
 
     initialize: function ( startTime, finishTime ) {
         this._startTime = startTime;
-        this._finishTime = finishTime;
-        this._beginTime = startTime;
-        this._finishTime = finishTime;
-        this._delay = this.DELAY;
+		this._finishTime = finishTime;
+		this._gapTime = finishTime - startTime;
+		this._beginTime = 0;
         this._interval = null;
-        this._running = false;
 
         this._handler = $B.bind(function (e) {
             var dday = this._getTime();
@@ -38,52 +36,57 @@ ixBand.utils.RemainTimer = $B.Class.extend({
     // ===============	Public Methods	=============== //
 
     /** Timer 시작, 이미 실행중이면 다시 시작하지 않는다. */
-    start: function () {
-        if ( !this._interval ) this._interval = setInterval( this._handler, this._delay );
-        this._running = true;
+	start: function () {
+		if ( !this._interval ) {
+			this._beginTime = new Date().getTime()
+			this._interval = setInterval(this._handler, this.DELAY);
+		}
+        
         return this;
     },
-    /** Timer 정지, currentCount는 재설정 하지 않는다. */
+    /** Timer 정지, delay를 재설정 하지 않는다. */
     stop: function () {
         if ( this._interval ) clearInterval( this._interval );
         this._interval = null;
-        this._running = false;
         return this;
     },
-    /** Stop후 currentCount = 0
+    /** Stop후 재설정
      * @param	{Number}	startTime		시작시간 재설정, UTC milisecond형식, 설정하지 않으면 처음 설정했던 시작시간으로 되돌아 간다.
      * @param	{Number}	finishTime		완료시간 재설정, UTC milisecond형식, 설정하지 않으면 처음 설정했던 완료시간으로 되돌아 간다.
      * @return	this
      */
     reset: function ( startTime, finishTime ) {
         this.stop();
-        this._reset( startTime, finishTime );
+		this._startTime = (typeof startTime === 'number') ? startTime : this._startTime;
+		this._finishTime = (typeof finishTime === 'number') ? finishTime : this._finishTime;
+		this._gapTime = this._finishTime - this._startTime;
         return this;
     },
     /** 타이머가 실행 중이면 true반환 */
     running: function () {
-        return this._running;
+		return this._interval;
     },
 
     // ===============	Private Methods	=============== //
 
     _getTime: function () {
-        var day, hour, minute, second,
-            gapTime = this._finishTime - ( this._beginTime += this.DELAY );
+		var day, hour, minute, second;
 
-        if ( gapTime <= 0 ) {
-            return {_gap: 0, day: 0, hour: 0, minute: 0, second: 0};
+		this._gapTime -= this._delay();
+		this._beginTime = new Date().getTime();
+
+		if ( this._gapTime <= 0 ) {
+            return { _gap: 0, day: 0, hour: 0, minute: 0, second: 0 };
         } else {
-            day = Math.floor( gapTime / 1000 / 60 / 60 / 24 );
-            hour = Math.floor( gapTime / 1000 / 60 / 60 - (24 * day) );
-            minute = Math.floor( gapTime / 1000 / 60 - (24 * 60 * day ) - (60 * hour) );
-            second = Math.floor( gapTime / 1000 - (24 * 60 * 60 * day) - (60 * 60 * hour) - (60 * minute) );
-            return {_gap: gapTime, day: day, hour: hour, minute: minute, second: second};
+			day = Math.floor( this._gapTime / 1000 / 60 / 60 / 24 );
+			hour = Math.floor( this._gapTime / 1000 / 60 / 60 - (24 * day) );
+			minute = Math.floor( this._gapTime / 1000 / 60 - (24 * 60 * day ) - (60 * hour) );
+			second = Math.floor( this._gapTime / 1000 - (24 * 60 * 60 * day) - (60 * 60 * hour) - (60 * minute) );
+			return { _gap: this._gapTime, day: day, hour: hour, minute: minute, second: second };
         }
     },
-
-    _reset: function ( begin, finish ) {
-        this._beginTime = ( typeof begin === 'number' )? begin : this._startTime;
-        this._finishTime = ( typeof finish === 'number' )? finish : this._finishTime;
-    }
+	
+	_delay: function () {
+		return new Date().getTimer() - this._beginTime;
+	}
 }, '$B.utils.RemainTimer');
